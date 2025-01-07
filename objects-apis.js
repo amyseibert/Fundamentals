454b250932259ee2205ed1db398a2c21a1b6cf65
Amy Seibert <amyeseibert@gmail.com>
2025-01-07 14:34:54 -0700

    Added variables-and-data.js file, conditionals-and-functions.js file, and objects-apis.js file.

---
+++ objects-apis.js
@@ -0,0 +1,24 @@
+/*
+*Objects, Interface, and API's
+ */
+
+let dog = {
+    name: "Steve",
+    color: "brown",
+    breed: "Pug",
+    size: "small",
+    bark: function(typeOfBark){
+        console.log("Bark!");
+    },
+};
+
+function x(y) {
+    y.num = y.num + 5;
+    console.log(y);
+}
+let y = {
+    name: "Tom",
+    num: 10,
+}
+x(y);
+console.log(y);
